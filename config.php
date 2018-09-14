4a11f7d34a1be039382f4fbf679b2a39f2152939
Shayan Mandegarian <smandegarian@yahoo.com>
2018-09-14 14:36:42 -0700

    changed to tomtom

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 $key = 'AIzaSyBgFEGhTyZ7Qhd955EtDx-xrBZRyIPK7Rs';
+$tomKey = 'NQtxNnNmww5RLcs9cwJBJzLmq6liWhC8';
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'config.php';
-global $db_host, $db_user, $db_name, $db_pass, $key;
+global $db_host, $db_user, $db_name, $db_pass, $key, $tomKey;
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Error " . mysqli_error($conn));
 $conn->set_charset("latin1");
 ini_set("allow_url_fopen", 1);
@@ -10,8 +10,8 @@ error_reporting(E_ALL ^ E_WARNING);
 $url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 $urlKey = '&key='.$key;
 
-$govUrl = 'https://geocoding.geo.census.gov/geocoder/locations/address?';
-$govSuffix = '&state=CA&benchmark=Public_AR_Current&format=json';
+$tomUrl = 'https://api.tomtom.com/search/2/geocode/';
+$tomSuffix = '.json?countrySet=US&lat=37.337&lon=-121.89&topLeft=37.553%2C-122.453&btmRight=37.4%2C-122.55&extendedPostalCodesFor=Addr%2CPAD%2CPOI&key='.$tomKey;
 
 $query = "SELECT id,address_1, address_2, city, state, zip_code FROM t_regist ORDER BY id ASC";
 $result = mysqli_query($conn, $query);
@@ -44,10 +44,13 @@ $result = mysqli_query($conn, $query);
             <h2>Coordinates</h2>
           </div>
           <div class="row">
-            <h5>Click on values to open to coords on Google Maps, <b>bold</b> coords means they might be incorrect (partial_match == true, or address isn't in gov census)</h5>
+            <h6>Click on values to open to coords on Google Maps, <b><i>bold</i></b> rows means they might be incorrect (Google and TomTom results are very different)</h6>
           </div>
           <div class="row">
-            <h5>Verified row is comparing Google Maps result with Government Geocoding service with a small margin of error</h5>
+            <h6>Verified row is comparing Google Geocoder result with TomTom Geocoder with a small margin of error</h6>
+          </div>
+          <div class="row">
+            <h6>Click on values in the Tom Lat / Tom Lon columns to view the coordinates from TomTom in Google Maps</h6>
           </div>
           <div class="page-hedaer clearfix">
             <table class="table-striped table-hover table">
@@ -57,10 +60,12 @@ $result = mysqli_query($conn, $query);
                   <th scope="col">Address 1</th>
                   <th scope="col">Address 2</th>
                   <th scope="col">City</th>
-                  <th scope="col">Latitude</th>
-                  <th scope="col">Longitude</th>
+                  <th scope="col">Google Lat</th>
+                  <th scope="col">Google Lon</th>
                   <th scope="col">Verified</th>
-                  <!-- <th scope="col">URL</th> -->
+                  <th scope="col">Tom Lat</th>
+                  <th scope="col">Tom Lon</th>
+                  <th scope="col">URL</th>
                 </tr>
               </thead>
               <tbody>
@@ -69,66 +74,74 @@ $result = mysqli_query($conn, $query);
 while ($row = mysqli_fetch_array($result)) {
   if ($row['address_2'] == '') {
     $address = $row['address_1'] . "," . $row['city'] . "," . $row['state'] . "," . $row['zip_code'];
-    $street = 'street='.urlencode($row['address_1']);
-    $city = '&city='.$row['city'];
+    $tomSearch = urlencode($row['address_1']) . ','. urlencode($row['city']) . ',' . 'CA';
+    // $street = 'street='.urlencode($row['address_1']);
+    // $city = '&city='.$row['city'];
     if ($row['city'] == "Redwood Shores") {
-      $city = '&city=Redwood City';
+      $city = 'Redwood City';
+      $tomSearch = urlencode($row['address_1']) . ',' . urlencode($city) . ',' . 'CA';
     }
     $addr = 1;
   }
   else {
     $address = $row['address_2'] . "," . $row['city'] . "," . $row['state'] . "," . $row['zip_code'];
     $addr = 2;
-    $street = 'street='.urlencode($row['address_2']);
-    $city = '&city='.$row['city'];
+    $tomSearch = urlencode($row['address_2']) . ','. urlencode($row['city']) . ',' . 'CA';
+    // $street = 'street='.urlencode($row['address_2']);
+    // $city = '&city='.$row['city'];
     if ($row['city'] == "Redwood Shores") {
-      $city = '&city=Redwood City';
+      $city = 'Redwood City';
+      $tomSearch = urlencode($row['address_2']) . ',' . urlencode($city) . ',' . 'CA';
     }
   }
-  $govAddress = $govUrl . $street . $city . $govSuffix;
-  // $govFull = urlencode($govAddress);
+  $tomAddress = $tomUrl . $tomSearch . $tomSuffix;
+  // $tomFull = urlencode($tomAddress);
   $urlAddress = urlencode($address);
   // $urlAddress = $address;
   $urlFull = $url . $urlAddress . $urlKey;
   $json = file_get_contents($urlFull);
   $obj = json_decode($json, true);
-  $govJson = file_get_contents($govAddress);
-  $govObj = json_decode($govJson, true);
-  if (isset($govObj['result']['addressMatches'][0]['coordinates']['x'])) {
-    $govLng = $govObj['result']['addressMatches'][0]['coordinates']['x'];
-    $govLat = $govObj['result']['addressMatches'][0]['coordinates']['y'];
+  $tomJson = file_get_contents($tomAddress);
+  $tomObj = json_decode($tomJson, true);
+  if (isset($tomObj['results'][0]['position']['lat'])) {
+    $tomLng = $tomObj['results'][0]['position']['lon'];
+    $tomLat = $tomObj['results'][0]['position']['lat'];
   }
 
   if ($obj['results'][0]['address_components'][0]['types'][0] == 'locality' || $obj['results'][0]['address_components'][0]['types'][0] == 'postal_code') {
     if ($addr == 1) {
       $address = $row['address_2'] . "," . $row['city'] . "," . $row['state'] . "," . $row['zip_code'];
-      $street = 'street='.urlencode($row['address_2']);
-      $city = '&city='.$row['city'];
+      $tomSearch = urlencode($row['address_2']) . ','. urlencode($row['city']) . ',' . 'CA';
+      // $street = 'street='.urlencode($row['address_2']);
+      // $city = '&city='.$row['city'];
       if ($row['city'] == "Redwood Shores") {
-        $city = '&city=Redwood City';
+        $city = 'Redwood City';
+        $tomSearch = urlencode($row['address_2']) . ',' . urlencode($city) . ',' . 'CA';
       }
     }
     else {
       $address = $row['address_1'] . "," . $row['city'] . "," . $row['state'] . "," . $row['zip_code'];
-      $street = 'street='.urlencode($row['address_1']);
-      $city = '&city='.$row['city'];
+      $tomSearch = urlencode($row['address_1']) . ','. urlencode($row['city']) . ',' . 'CA';
+      // $street = 'street='.urlencode($row['address_1']);
+      // $city = '&city='.$row['city'];
       if ($row['city'] == "Redwood Shores") {
-        $city = '&city=Redwood City';
+        $city = 'Redwood City';
+        $tomSearch = urlencode($row['address_1']) . ',' . urlencode($city) . ',' . 'CA';
       }
     }
-    $govAddress = $govUrl . $street . $city . $govSuffix;
+    $tomAddress = $tomUrl . $tomSearch . $tomSuffix;
     $urlAddress = urlencode($address);
     // $urlAddress = $address;
     $urlFull = $url . $urlAddress . $urlKey;
     $json = file_get_contents($urlFull);
     $obj = json_decode($json, true);
-    if ($street != 'street=') {
-      $govJson = file_get_contents($govAddress);
-      $govObj = json_decode($govJson, true);
-    }
-    if (isset($govObj['result']['addressMatches'][0]['coordinates']['x'])) {
-      $govLng = $govObj['result']['addressMatches'][0]['coordinates']['x'];
-      $govLat = $govObj['result']['addressMatches'][0]['coordinates']['y'];
+    // if ($street != 'street=') {
+    $tomJson = file_get_contents($tomAddress);
+    $tomObj = json_decode($tomJson, true);
+    // }
+    if (isset($tomObj['results'][0]['position']['lon'])) {
+      $tomLng = $tomObj['results'][0]['position']['lon'];
+      $tomLat = $tomObj['results'][0]['position']['lat'];
     }
     if ($obj['results'][0]['address_components'][0]['types'][0] == 'locality' || $obj['results'][0]['address_components'][0]['types'][0] == 'postal_code') {
       continue;
@@ -137,29 +150,17 @@ while ($row = mysqli_fetch_array($result)) {
   $ver = 'No';
   $lat = $obj['results'][0]['geometry']['location']['lat'];
   $lng = $obj['results'][0]['geometry']['location']['lng'];
-  $latDiff = abs($lat - $govLat);
-  $lngDiff = abs($lng - $govLng);
+  $latDiff = abs($lat - $tomLat);
+  $lngDiff = abs($lng - $tomLng);
   if (($latDiff <= 0.01) && ($lngDiff <= 0.01)) {
     $ver = 'Yes';
   }
   // if (($latDiff >= 0.02) || ($lngDiff >= 0.02)) {
-  //   // $lat = $govLat;
-  //   // $lng = $govLng;
+  //   // $lat = $tomLat;
+  //   // $lng = $tomLng;
   // }
   $link = 'https://www.google.com/maps/search/?api=1&query='.$lat.','.$lng;
-  echo "<tr>";
-  echo "<td>";
-  echo "<a href=$link>".$row['id']."</a>";
-  echo "</td>";
-  echo "<td>";
-  echo "<a href=$link>".$row['address_1']."</a>";
-  echo "</td>";
-  echo "<td>";
-  echo "<a href=$link>".$row['address_2']."</a>";
-  echo "</td>";
-  echo "<td>";
-  echo "<a href=$link>".$row['city']."</a>";
-  echo "</td>";
+  $tomLink = 'https://www.google.com/maps/search/?api=1&query='.$tomLat.','.$tomLng;
 
   $urlAddress = urlencode($address);
   // $urlAddress = $address;
@@ -167,21 +168,42 @@ while ($row = mysqli_fetch_array($result)) {
   $json = file_get_contents($urlFull);
   $obj = json_decode($json, true);
 
+  // (isset($obj['results'][0]['partial_match']))
+  // && $obj['results'][0]['partial_match'] == true ||
+
   if ($obj['status'] == "OK") {
-    if ((isset($obj['results'][0]['partial_match']))
-    && $obj['results'][0]['partial_match'] == true || $ver == 'No') {
+    if ($ver == 'No') {
       $lat = $obj['results'][0]['geometry']['location']['lat'];
       $lng = $obj['results'][0]['geometry']['location']['lng'];
-      $latDiff = abs($lat - $govLat);
-      $lngDiff = abs($lng - $govLng);
+      $tomLng = $tomObj['results'][0]['position']['lon'];
+      $tomLat = $tomObj['results'][0]['position']['lat'];
+      $latDiff = abs($lat - $tomLat);
+      $lngDiff = abs($lng - $tomLng);
       $ver = 'No';
       if (($latDiff <= 0.01) && ($lngDiff <= 0.01)) {
         $ver = 'Yes';
       }
       // if (($latDiff >= 0.02) || ($lngDiff >= 0.02)) {
-      //   // $lat = $govLat;
-      //   // $lng = $govLng;
+      //   // $lat = $tomLat;
+      //   // $lng = $tomLng;
       // }
+      echo "<tr>";
+      echo "<td><b><i>";
+      // echo "<a href=$link>".$row['id']."</a>";
+      echo $row['id'];
+      echo "</b></i></td>";
+      echo "<td><b><i>";
+      // echo "<a href=$link>".$row['address_1']."</a>";
+      echo $row['address_1'];
+      echo "</b></i></td>";
+      echo "<td><b><i>";
+      // echo "<a href=$link>".$row['address_2']."</a>";
+      echo $row['address_2'];
+      echo "</b></i></td>";
+      echo "<td><b><i>";
+      // echo "<a href=$link>".$row['city']."</a>";
+      echo $row['city'];
+      echo "</b></i></td>";
       echo "<td><b><i>";
       echo "<a href=$link>".$lat."</a>";
       echo "</b></i></td>";
@@ -189,22 +211,48 @@ while ($row = mysqli_fetch_array($result)) {
       echo "<a href=$link>".$lng."</a>";
       echo "</b></i></td>";
       echo "<td><b><i>";
-      echo "<a href=$link>".$ver."</a>";
+      // echo "<a href=$link>".$ver."</a>";
+      echo $ver;
+      echo "</b></i></td>";
+      echo "<td><b><i>";
+      echo "<a href=$tomLink>".$tomLat."</a>";
+      echo "</b></i></td>";
+      echo "<td><b><i>";
+      echo "<a href=$tomLink>".$tomLng."</a>";
       echo "</b></i></td>";
     }
     else {
       $lat = $obj['results'][0]['geometry']['location']['lat'];
       $lng = $obj['results'][0]['geometry']['location']['lng'];
-      $latDiff = abs($lat - $govLat);
-      $lngDiff = abs($lng - $govLng);
+      $latDiff = abs($lat - $tomLat);
+      $lngDiff = abs($lng - $tomLng);
+      $tomLng = $tomObj['results'][0]['position']['lon'];
+      $tomLat = $tomObj['results'][0]['position']['lat'];
       $ver = 'No';
       if (($latDiff <= 0.01) && ($lngDiff <= 0.01)) {
         $ver = 'Yes';
       }
       // if (($latDiff >= 0.02) || ($lngDiff >= 0.02)) {
-      //   // $lat = $govLat;
-      //   // $lng = $govLng;
+      //   // $lat = $tomLat;
+      //   // $lng = $tomLng;
       // }
+      echo "<tr>";
+      echo "<td>";
+      // echo "<a href=$link>".$row['id']."</a>";
+      echo $row['id'];
+      echo "</td>";
+      echo "<td>";
+      // echo "<a href=$link>".$row['address_1']."</a>";
+      echo $row['address_1'];
+      echo "</td>";
+      echo "<td>";
+      // echo "<a href=$link>".$row['address_2']."</a>";
+      echo $row['address_2'];
+      echo "</td>";
+      echo "<td>";
+      // echo "<a href=$link>".$row['city']."</a>";
+      echo $row['city'];
+      echo "</td>";
       echo "<td>";
       echo "<a href=$link>".$lat."</a>";
       echo "</td>";
@@ -212,7 +260,14 @@ while ($row = mysqli_fetch_array($result)) {
       echo "<a href=$link>".$lng."</a>";
       echo "</td>";
       echo "<td>";
-      echo "<a href=$link>".$ver."</a>";
+      // echo "<a href=$link>".$ver."</a>";
+      echo $ver;
+      echo "</td>";
+      echo "<td>";
+      echo "<a href=$tomLink>".$tomLat."</a>";
+      echo "</td>";
+      echo "<td>";
+      echo "<a href=$tomLink>".$tomLng."</a>";
       echo "</td>";
     }
 
@@ -222,7 +277,7 @@ while ($row = mysqli_fetch_array($result)) {
     echo "<td>----</td>";
   }
 
-  // echo "<td>".$urlFull."</td>";
+  echo "<td>".$tomAddress."</td>";
   echo "</tr>";
 } ?>
               </tbody>
